dcba0e385e7b979cdb59ac34f7faf6ce6e0f6850
karlkras <karlkras@yahoo.com>
2024-02-03 17:14:42 -0800

    start of section 5 of the course.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import Input from "./component/Input.tsx";
-import Form, { type FormHandle } from "./component/Form.tsx";
-import Button from "./component/Button.tsx";
-import { useRef } from "react";
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
 function App() {
-  const customFormRef = useRef<FormHandle>(null);
-
-  const handleFormSave = (value: unknown) => {
-    const extractData = value as {
-      "first-name": string,
-      "last-name": string, age: string
-    }
-    console.log(extractData);
-    customFormRef.current?.clear();
-  }
-
-
   return (
-
-    <main>
-      <Form onSave={handleFormSave} ref={customFormRef}>
-        <Input
-          label="First Name"
-          id="first-name"
-          type="text"
-          required
-        />
-        <Input label="Last Name" id="last-name" type="text"
-               title='Entering Last Name is required'
-               required/>
-        <Input label="Age" id="age" type="number"
-               required/>
-        <p>
-          <Button className="button" el="button">Save</Button>
-        </p>
-      </Form>
-    </main>
-  )
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
+  );
 }
 
 export default App;

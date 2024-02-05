57c89c632d5947c71da28165308941f25fdd9a2b
karlkras <karlkras@yahoo.com>
2024-02-04 16:35:39 -0800

    some issue with timers resetting on stop.

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,40 @@
 import Container from './UI/Container.tsx';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import { type Timer as TimerProps, useTimersContext } from '../store/timers-context.tsx';
+import { useEffect, useRef, useState } from "react";
 
-const Timer = ({ name, duration }:TimerProps ) => {
+const Timer = ({name, duration}: TimerProps) => {
+  const intervalRef = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
+  const timeInterval = 50;
+
+  if (remainingTime <= 0) {
+    if (intervalRef.current)
+      clearInterval(intervalRef.current);
+  }
+
+  useEffect(() => {
+    let interval: number;
+    if(isRunning) {
+      interval = window.setInterval(() => {
+        setRemainingTime((prevTime) => prevTime - timeInterval)
+      }, timeInterval);
+      intervalRef.current = interval;
+    } else if (intervalRef.current) {
+      clearInterval(intervalRef.current)
+    }
+    return () => clearInterval(interval)
+  }, [isRunning]);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime}/>
+      </p>
+      <p>Time Remaining {formattedRemainingTime}</p>
     </Container>
   );
 }
